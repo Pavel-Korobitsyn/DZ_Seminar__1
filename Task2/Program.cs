f996d7f236894a4090601deef00398b6a955ee42
PC <pkorobisyn123@gmail.com>
2022-09-03 13:12:40 +0500

    Выполнили задание 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите число 1.");
+string num1 = Console.ReadLine ();
+Console.WriteLine ("Введите число 2.");
+string num2 = Console.ReadLine ();
+
+int intNum1 = int.Parse(num1);
+int intNum2 = int.Parse(num2);
+
+if (intNum1 > intNum2)
+    {
+        Console.WriteLine($"Максимальное число равно: {intNum1}");
+    }
+else
+    {
+        Console.WriteLine($"Максимальное число равно: {intNum2}");
+    }
