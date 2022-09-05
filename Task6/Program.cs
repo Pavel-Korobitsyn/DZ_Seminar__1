8b93b057cc700d589a1ecd11bb0b8d90e073f77e
PC <pkorobisyn123@gmail.com>
2022-09-05 21:34:50 +0500

    Добавили программу с решением задания6

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine();
+
+double doubleNum = double.Parse(num);
+
+if (doubleNum % 2 == 0)
+    {
+        Console.WriteLine("Число чётное");
+    }
+else
+    {
+        Console.WriteLine("Число не чётное");
+    }
