839a8dd6e77f6a08172fe0bd97fcb42282efd59c
PC <pkorobisyn123@gmail.com>
2022-09-05 21:56:03 +0500

    Добавили программу к заданию 8

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string N = Console.ReadLine();
+
+int intN = int.Parse(N);
+
+Console.Write($"Чётные числа в промежутке от 1 до {intN}: " );
+ 
+for(int i = 1; i < intN; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i} ");
+        }
+    }
